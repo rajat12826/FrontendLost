10fd5d4deba55dad27bd15c78297f0e189ad44e4
rajat12826 <pariharrajat078@gmail.com>
2024-10-18 21:54:31 +0530

    updated login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Login from "./Login";
 
 function Navbar() {
+    const user=localStorage.getItem('user')
     return (
         <nav>
-            <ul className="flex justify-between bg-gray-800 text-white font-semibold text-md py-5 px-10 max-sm:px-0 ">
+            {
+                user?<ul className="flex justify-between bg-gray-800 text-white font-semibold text-md py-5 px-10 max-sm:px-0 ">
                 <li><Link to="/" className="hover:font-bold hover:text-green-500 ">Home</Link></li>
                 <li><Link to="/login" className="hover:font-bold hover:text-green-500 ">Login/Register</Link></li>
                 <li><Link to="/find" className="hover:font-bold hover:text-green-500 ">Find</Link></li>
@@ -12,7 +15,8 @@ function Navbar() {
             
                 <li><Link to="/items" className="hover:font-bold hover:text-green-500 ">Items</Link></li>
                 <li><Link to="/match" className="hover:font-bold hover:text-green-500 ">Match</Link></li>
-            </ul>
+            </ul>:<Login/>
+            }
         </nav>
     );
 }
