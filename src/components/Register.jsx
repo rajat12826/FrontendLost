31b50662b9d60cd054b9a56b71f0e4c4ae1e9e7c
rajat12826 <pariharrajat078@gmail.com>
2024-10-18 23:00:40 +0530

    added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 import { useNavigate } from 'react-router-dom'
-import { registerAPI } from '../Api/Api'
+import { registerAPI } from './Api'
 import axios from 'axios'
 function Register() {
     
